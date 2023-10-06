0e5d333b16a8228a7486c0a3bf8a760f52a290bd
chimny04 <lucia.roback@gmail.com>
2023-10-06 20:48:02 +0900

    4-4

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int year;
+    
+    printf("input a year: ");
+    scanf("%d", &year);
+    
+    if ( (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 )
+        printf("This is leap year\n");
+    else
+        printf("This is not leap year\n");
+  
+    system("PAUSE");	
+    return 0;
+}
